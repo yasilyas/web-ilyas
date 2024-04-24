68b7b619e711105054886cbcba3b4a63b4539297
Ilyas-Pebriyan <123224604+yasilyas@users.noreply.github.com>
2024-04-24 14:02:08 +0700

    first commit

---
+++ index2.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SMK ATLANTIS PLUS</title>
+
+    <!--Font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,700;1,400;1,700&display=swap"
+        rel="stylesheet" />
+    <!--End Font-->
+    <!--link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+
+    <!--Feather-->
+    <script src="https://unpkg.com/feather-icons"></script>
+    <!--End Feather-->
+
+    <link rel="stylesheet" href="style2.css" />
+</head>
+
+<body>
+    <!--navbar-->
+    <nav class="navbar">
+        <a href="#" class="navbar-logo">SMK ATLANTIS PLUS
+            <img src="SMK_AES.png" style="width: 20px; height: 20px" /></a>
+
+        <div class="navbar-nav">
+            <a href="#home">Beranda</a>
+            <a href="#about">Tentang</a>
+            <a href="index.php">Quiz</a>
+            <a href="#staff">Staff</a>
+            <a href="#kontak">Kontak</a>
+        </div>
+
+        <div class="navbar-extra">
+            <!--search-->
+            <a href="#" id="hamburger-menu"> <i data-feather="menu"></i></a>
+        </div>
+    </nav>
+
+    <!--hero-->
+    <section class="hero" id="home">
+        <main class="content">
+            <h1>SMK ATLANTIS PLUS</h1>
+            <p>Sekolah Mendepankan Murid Untuk Maju</p>
+            <a href="index.php" class="cta">Mulai</a>
+        </main>
+    </section>
+
+    <!--About-->
+    <section id="about" class="about">
+        <h2>Tentang Kami</h2>
+
+        <div class="row">
+            <div class="about-img">
+                <img src="10.jpg" alt="Tentang Kami" />
+            </div>
+            <div class="content">
+                <h3>SMK ATLANTIS PLUS</h3>
+                <p>
+                    SMK ATLANTIS Plus merancang kurikulum berbasis industri dengan
+                    konsep 50% menggunakan kurikulum nasional dan 50% menggunakan
+                    kurikulum industri dengan mendatangkan praktisi-praktisi industri
+                    sebagai fasilitator.
+                </p>
+                <p>
+                    <span>></span>SMK yang mampu menciptakan sumber daya manusia yang
+                    berkualitas Tinggi, Religius, Berkarakter, Profesional dan Mandiri.
+                </p>
+
+                <p>
+                    <span>></span>Mampu berkarya dan kreatif dalam kehidupan
+                    bermasyarakat, berbangsa dan bernegara.
+                </p>
+
+                <p>
+                    Menciptakan Generasi Muda Terdidik, Berkualitas Tinggi, Keatif,
+                    Berkarakter dan Mandiri, Menguasai Ilmu Pengetahuan dan Tekhnologi,
+                    serta Active Language yang mampu Berkarya dalam kehidupan
+                    bermasyarakat, berbangsa dan bernegara.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!--Staff-->
+    <section id="staff" class="staff">
+        <div class="row1">
+            <h1>GURU</h1>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <div class="img-container">
+                        <img src="42.jpg" />
+                    </div>
+                    <h3>Bu Wardah</h3>
+                    <p>Kepala Sekolah</p>
+                    <div class="icons">
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="#"><i class="fa-brands fa-github"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="img-container">
+                        <img src="41.jpeg" />
+                    </div>
+                    <h3>Bu chichi</h3>
+                    <p>Bidang Kurikulum</p>
+                    <div class="icons">
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="#"><i class="fa-brands fa-github"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="img-container">
+                        <img src="43.jpeg" />
+                    </div>
+                    <h3>Pak Renaldi</h3>
+                    <p>Kesiswaan</p>
+                    <div class="icons">
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="#"><i class="fa-brands fa-github"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="img-container">
+                        <img src="44.jpeg" />
+                    </div>
+                    <h3>Bu Nisa</h3>
+                    <p>Hubungan Industri</p>
+                    <div class="icons">
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="#"><i class="fa-brands fa-github"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="kontak" class="contact">
+        <h2>Kontak Kami</h2>
+        <p>
+        </p>
+        <div class="row">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.9810489207116!2d106.77324067326579!3d-6.396443462557887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69e966f00b8b2b%3A0x468862342db39278!2sSMP%20%26%20SMK%20ATLANTIS%20PLUS!5e0!3m2!1sid!2sid!4v1694966825393!5m2!1sid!2sid"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+            <form action="https://formsubmit.co/506b1b2042ddc167cf2e472e5a4a4f34" method="post">
+                <div class="input">
+                    <i data-feather="user"></i>
+                    <input type="name" name="name" placeholder="nama" required />
+                </div>
+                <div class="input">
+                    <i data-feather="mail"></i>
+                    <input type="email" name="email" placeholder="email" required />
+                </div>
+                <div class="input">
+                    <i data-feather="massage-square"></i>
+                    <input textarea type="massage" name="massage" placeholder="Kesan dan Saran" required></textarea>
+                </div>
+                <button type="submit" name="submit" class="btn">Kirim Pesan</button>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <div class="social">
+            <a href="#"><i data-feather="instagram"></i></a>
+            <a href="#"><i data-feather="twitter"></i></a>
+            <a href="#"><i data-feather="facebook"></i></a>
+            <a href="#"><i data-feather="youtube"></i></a>
+        </div>
+        <div class="link">
+            <a href="#home">Beranda</a>
+            <a href="#about">Tentang</a>
+            <a href="#index.php">Quiz</a>
+            <a href="#staff">Staff</a>
+            <a href="#kontak">Kontak</a>
+        </div>
+        <div class="credit">
+            <p>Crated by <a href>Grup Bajak Laut Topi Jerami</a>. | &copy; 2023.</p>
+        </div>
+    </footer>
+
+    <!--feather-->
+    <script>
+    feather.replace();
+    </script>
+
+    <!--Js-->
+    <script src="script.js"></script>
+</body>
+
+</html>
